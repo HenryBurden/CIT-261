dedd40c5d2749deaf12353a9482250e725569933
HenryBurden <35981283+HenryBurden@users.noreply.github.com>
2019-06-25 09:32:16 -0600

    Add files via upload

---
+++ Team10/utilities.js
@@ -0,0 +1,27 @@
+async function getJSON(url) {
+    try{
+        const response = await fetch(url);
+        if(!response.ok) {
+            throw Error(response.statusText);
+        }
+        else {
+        const json = await response.json();
+        //console.log(response);
+        return json;
+        }
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+export async function getQuakes(url){
+    return await getJSON(url);
+}
+
+export const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+};
+
